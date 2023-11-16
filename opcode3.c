5baad8337b5bd610ccd4f44b13776979ea879dc1
pasebukky <olubukunolapase@gmail.com>
2023-11-16 09:33:48 +0100

    Free stack for pchar opcode

--- opcode3.c
+++ opcode3.c
@@ -46,10 +46,10 @@ void x_pchar(mstack_t **stack, unsigned int line_number)
 {
 mstack_t *top;
 
-if (stack == NULL || *stack == NULL)
+if (*stack == NULL)
 {
 	fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
-	free_stack(*stack);
+	/* free_stack(*stack); */
 	exit(EXIT_FAILURE);
 }
 
@@ -62,7 +62,7 @@ if (isalpha(top->n) == 0)
 {
 	fprintf(stderr, "L%d: can't pchar, value not an alphabetic character\n",
 	line_number);
-	free_stack(*stack);
+	/* free_stack(*stack); */
 	exit(EXIT_FAILURE);
 }
 printf("%c\n", top->n);
